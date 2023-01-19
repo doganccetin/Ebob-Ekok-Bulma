9bd56ee8153d663807665714e95b073eaa5b06dd
ask0 <voidask0@gmail.com>
2023-01-19 22:15:39 +0300

    Update EbobAndEkok.java
    Fixed ekok

--- EbobveEkok/src/EbobAndEkok.java
+++ EbobveEkok/src/EbobAndEkok.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class EbobAndEkok {
     public static void main(String[] args) {
-
+        
         Scanner sc = new Scanner(System.in);
         int say1, say2, gcd = 1;
 
@@ -11,13 +11,15 @@ public class EbobAndEkok {
         System.out.print("İkinci sayıyı girin: ");
         say2 = sc.nextInt();
 
+        int say1p = say1;
+        int say2p = say2;
         while (say2 != 0) {
             int temp = say2;
             say2 = say1 % say2;
             say1 = temp;
         }
         int ebob = say1;
-        int ekok = (say1 * say2) / ebob;
+        int ekok = (say1p * say2p) / ebob;
         System.out.println("Ebob: " + ebob);
         System.out.println("Ekok: " + ekok);
     }
